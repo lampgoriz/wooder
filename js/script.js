06e186d64fc7990d84680d7953f157704c67499b
lampogriz <potolochni@gmail.com>
2020-07-13 16:07:21 +0300

    last validation check at w3c and improving html

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
-let header = document.getElementById('header');
 let menu__toggle_button = document.getElementById('menu__toggle-button');
 let nav__menu_list = document.getElementById('nav__menu-list');
+let nav__menu_list_items = document.getElementsByClassName('nav__menu__list-item');
+// let yOffsetOfTop = document.
 
 menu__toggle_button.addEventListener('click', function () {
-    console.log('click');
+    // console.log('click');
     menu__toggle_button.classList.toggle('menu__toggle-button-active');
     nav__menu_list.classList.toggle('nav__menu__list-active');
+    for(let i = 0; i <nav__menu_list_items.length; i++){
+        nav__menu_list_items[i].classList.toggle('nav__menu__list-item-active');
+    }
 });
 
+function smoothScroll() {
+
+}
 
 
 
