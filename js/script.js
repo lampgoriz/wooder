0b2ab43eba50ee763d575b3834ff816d1b85decd
lampogriz <potolochni@gmail.com>
2021-06-01 14:08:07 +0300

    add logo

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
+//toggle menu//
 let menu__toggle_button = document.getElementById('menu__toggle-button');
 let nav__menu_list = document.getElementById('nav__menu-list');
 let nav__menu_list_items = document.getElementsByClassName('nav__menu__list-item');
-// let yOffsetOfTop = document.
 
 menu__toggle_button.addEventListener('click', function () {
-    // console.log('click');
     menu__toggle_button.classList.toggle('menu__toggle-button-active');
     nav__menu_list.classList.toggle('nav__menu__list-active');
     for(let i = 0; i <nav__menu_list_items.length; i++){
@@ -12,13 +11,39 @@ menu__toggle_button.addEventListener('click', function () {
     }
 });
 
-function smoothScroll() {
+//smooth scroll//
+function smoothScroll(target, duration) {
+    let Target = document.querySelector(target);
+    let targetPosition = Target.getBoundingClientRect();
+    let startPosition = window.pageYOffset;
+    let distance = targetPosition.top - startPosition;
+    let startTime = null;
 
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        let timeElapsed = currentTime - startTime;
+        let run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if(timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if(t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
 }
 
+nav__menu_list.addEventListener("click", getIdToSmoothScroll);
 
+function getIdToSmoothScroll(event) {
 
+    let clickedElement = event.target;
+    let elementIdToScroll = clickedElement.innerHTML;
 
-
-
+    smoothScroll('#' + elementIdToScroll, 1000);
+}
 
